d8ac8572021efdad22d1193c77a301e61adaee92
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-10-19 15:35:07 +0300

    avatar processed by jimp with size 250x250

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const jimp = require("jimp");
 
 const { User } = require("../../models/user");
 
@@ -9,11 +10,16 @@ const updateAvatar = async (req, res) => {
   try {
     const { _id } = req.user;
     const { path: tempUpload, originalname } = req.file;
-    const extention = originalname.split(".").pop();
+    // const extention = originalname.split(".").pop();
+    const [extention] = originalname.split(".").reverse();
     const filename = `${_id}.${extention}`;
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", filename);
+
+    const file = await jimp.read(resultUpload);
+    await file.resize(250, 250).write(resultUpload);
+
+    const avatarURL = path.join("avatars", resultUpload);
     await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({
       avatarURL,
